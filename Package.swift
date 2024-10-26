9ef8333beb5951ea15ba39df690cde4db25b611c
yutailang0119 <yutailang0119@gmail.com>
2024-10-27 02:15:06 +0900

    Fix platforms to update

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
   name: "swift-bypass-accessing",
-  platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+  platforms: [.macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8), .macCatalyst(.v15)],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
