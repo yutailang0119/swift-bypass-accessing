54c51c572113e28bb91319729513d7c43a62c375
yutailang0119 <yutailang0119@gmail.com>
2024-10-28 02:13:50 +0900

    Add BypassAccessMacro

---
+++ Sources/BypassAccessingMacros/BypassAccessMacro.swift
@@ -0,0 +1,12 @@
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+public struct BypassAccessMacro: PeerMacro {
+  public static func expansion(
+    of node: SwiftSyntax.AttributeSyntax,
+    providingPeersOf declaration: some SwiftSyntax.DeclSyntaxProtocol,
+    in context: some SwiftSyntaxMacros.MacroExpansionContext
+  ) throws -> [SwiftSyntax.DeclSyntax] {
+    []
+  }
+}
