f1848f78be1202125578cf611cd0a89af9eb0588
yutailang0119 <yutailang0119@gmail.com>
2024-10-30 23:04:29 +0900

    Fix BypassAccessTests to remove test case

--- Tests/BypassAccessingTests/BypassAccessTests.swift
+++ Tests/BypassAccessingTests/BypassAccessTests.swift
@@ -1,7 +1,5 @@
 import SwiftSyntaxMacroExpansion
 import SwiftSyntaxMacros
-import SwiftSyntaxMacrosTestSupport
-import XCTest
 
 #if canImport(BypassAccessingMacros)
 import BypassAccessingMacros
@@ -10,19 +8,3 @@ let testMacros: [String: Macro.Type] = [
   "BypassAccess": BypassAccessMacro.self
 ]
 #endif
-
-final class BypassAccessTests: XCTestCase {
-  func testBypassAccessMacro() throws {
-    #if canImport(BypassAccessingMacros)
-    assertMacroExpansion(
-      """
-      """,
-      expandedSource: """
-        """,
-      macros: testMacros
-    )
-    #else
-    throw XCTSkip("macros are only supported when running tests for the host platform")
-    #endif
-  }
-}
