47339547b72027869dde5ecddf70cc97390956b8
yutailang0119 <yutailang0119@gmail.com>
2024-11-03 01:03:05 +0900

    Fux AttributeListSyntax to add isMainActor

--- Sources/BypassAccessingMacros/Extensions.swift
+++ Sources/BypassAccessingMacros/Extensions.swift
@@ -1,1 +1,14 @@
 import SwiftSyntax
+
+extension AttributeListSyntax {
+  var isMainActor: Bool {
+    for attribute in self {
+      for token in attribute.tokens(viewMode: .all) {
+        if token.tokenKind == .identifier("MainActor") {
+          return true
+        }
+      }
+    }
+    return false
+  }
+}
