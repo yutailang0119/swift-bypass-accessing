803ef26f92fd23b0a4e5720145bc39806d0d9cba
yutailang0119 <yutailang0119@gmail.com>
2024-11-03 01:40:07 +0900

    Fix VariableDeclSyntax to add extensions

--- Sources/BypassAccessingMacros/Extensions.swift
+++ Sources/BypassAccessingMacros/Extensions.swift
@@ -25,3 +25,49 @@ extension DeclModifierListSyntax {
     return true
   }
 }
+
+extension VariableDeclSyntax {
+  private var identifierPattern: IdentifierPatternSyntax? {
+    bindings.first?.pattern.as(IdentifierPatternSyntax.self)
+  }
+
+  var identifier: TokenSyntax? {
+    bindings.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier
+  }
+
+  var type: TypeSyntax? {
+    bindings.first?.typeAnnotation?.type
+  }
+
+  func accessorsMatching(_ predicate: (TokenKind) -> Bool) -> [AccessorDeclSyntax] {
+    let accessors: [AccessorDeclListSyntax.Element] = bindings.compactMap { patternBinding in
+      switch patternBinding.accessorBlock?.accessors {
+      case .accessors(let accessors):
+        return accessors
+      default:
+        return nil
+      }
+    }.flatMap { $0 }
+    return accessors.compactMap { accessor in
+      if predicate(accessor.accessorSpecifier.tokenKind) {
+        return accessor
+      } else {
+        return nil
+      }
+    }
+  }
+
+  var isComputed: Bool {
+    if accessorsMatching({ $0 == .keyword(.get) }).count > 0 {
+      return true
+    } else {
+      return bindings.contains { binding in
+        if case .getter = binding.accessorBlock?.accessors {
+          return true
+        } else {
+          return false
+        }
+      }
+    }
+  }
+}
