0c676af3427455d352171da571dd1230ee985107
yutailang0119 <yutailang0119@gmail.com>
2024-11-02 23:43:03 +0900

    Fix BypassAcess to branch to suppurt declaration

--- Sources/BypassAccessingMacros/BypassAccessMacro.swift
+++ Sources/BypassAccessingMacros/BypassAccessMacro.swift
@@ -7,6 +7,14 @@ public struct BypassAccessMacro: PeerMacro {
     providingPeersOf declaration: some SwiftSyntax.DeclSyntaxProtocol,
     in context: some SwiftSyntaxMacros.MacroExpansionContext
   ) throws -> [SwiftSyntax.DeclSyntax] {
-    []
+    if let variable = declaration.as(VariableDeclSyntax.self) {
+      return []
+    } else if let initializer = declaration.as(InitializerDeclSyntax.self) {
+      return []
+    } else if let function = declaration.as(FunctionDeclSyntax.self) {
+      return []
+    } else {
+      return []
+    }
   }
 }
