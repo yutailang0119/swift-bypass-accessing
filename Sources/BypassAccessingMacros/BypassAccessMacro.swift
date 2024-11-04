397a4165f4d5889bc23316d59c83927c43f2ff7f
yutailang0119 <yutailang0119@gmail.com>
2024-11-04 22:26:29 +0900

    Fix FunctionDeclSyntax to implement output

--- Sources/BypassAccessingMacros/BypassAccessMacro.swift
+++ Sources/BypassAccessingMacros/BypassAccessMacro.swift
@@ -11,7 +11,36 @@ public struct BypassAccessMacro: PeerMacro {
     if let variable = declaration.as(VariableDeclSyntax.self) {
       return []
     } else if let function = declaration.as(FunctionDeclSyntax.self) {
-      return []
+      let staticModifier: TokenSyntax? = function.modifiers.isInstance ? nil : .keyword(.static)
+      let mainActorAttribute: AttributeSyntax? =
+        function.attributes.isMainActor ? AttributeSyntax(attributeName: TypeSyntax(stringLiteral: "MainActor")) : nil
+      let tryToken: TokenSyntax? = function.signature.effectSpecifiers.flatMap { $0.isThrows ? .keyword(.try) : nil }
+      let awaitToken: TokenSyntax? = function.signature.effectSpecifiers.flatMap { $0.isAsync ? .keyword(.await) : nil }
+
+      let functionDecl = try FunctionDeclSyntax(
+        """
+        \(mainActorAttribute) \(staticModifier)
+        func ___\(function.name)\(function.genericParameterClause)\(function.signature.trimmed) \(function.genericWhereClause){
+          \(tryToken) \(awaitToken) \(function.name)(
+            \(
+                raw: function.signature.parameterClause.parameters
+                    .map {
+                      let inoutToken: TokenSyntax? = $0.type.as(AttributedTypeSyntax.self).flatMap { $0.specifiers.isInout ? TokenSyntax(.prefixAmpersand, presence: .present) : nil }
+                      return "\($0.firstName.trimmed): \(inoutToken?.text ?? "")\($0.secondName?.trimmed ?? $0.firstName.trimmed)"
+                    }
+                    .joined(separator: ",\n")
+            )
+          )
+        }
+        """
+      )
+      return [
+        """
+        #if DEBUG
+        \(functionDecl.trimmed.formatted())
+        #endif
+        """
+      ]
     } else if let initializer = declaration.as(InitializerDeclSyntax.self) {
       return []
     } else {
