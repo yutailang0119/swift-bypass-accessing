906a896cb8901cb5a4b3c483cb884e1c2695a5f7
yutailang0119 <yutailang0119@gmail.com>
2024-11-11 00:13:19 +0900

    Fix InitializerExpansionTests to add testPlain

--- Tests/BypassAccessingTests/Expansions/InitializerExpansionTests.swift
+++ Tests/BypassAccessingTests/Expansions/InitializerExpansionTests.swift
@@ -2,4 +2,62 @@ import SwiftSyntaxMacroExpansion
 import SwiftSyntaxMacrosTestSupport
 import XCTest
 
-final class InitializerExpansionTests: XCTestCase {}
+final class InitializerExpansionTests: XCTestCase {
+  func testPlain() throws {
+    #if canImport(BypassAccessingMacros)
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private init() {}
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private init() {}
+
+          #if DEBUG
+          static
+          func ___init() -> Self {
+              Self.init(
+
+            )
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private init(name: String) {
+          print(name)
+        }
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private init(name: String) {
+            print(name)
+          }
+
+          #if DEBUG
+          static
+          func ___init(name: String) -> Self {
+              Self.init(
+              name: name
+            )
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+}
