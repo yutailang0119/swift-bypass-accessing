7b7f5aa699f704b92cf9a3d6d25d11e05c91a0d4
yutailang0119 <yutailang0119@gmail.com>
2024-11-12 23:52:00 +0900

    Fix PropertyExpansionTests to add testStatic

--- Tests/BypassAccessingTests/Expansions/PropertyExpansionTests.swift
+++ Tests/BypassAccessingTests/Expansions/PropertyExpansionTests.swift
@@ -128,4 +128,59 @@ final class PropertyExpansionTests: XCTestCase {
     throw XCTSkip("macros are only supported when running tests for the host platform")
     #endif
   }
+
+  func testStatic() throws {
+    #if canImport(BypassAccessingMacros)
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private static let name: String = "yutailang0119"
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private static let name: String = "yutailang0119"
+
+          #if DEBUG
+          static
+          var ___name: String {
+            name
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private static var name: String = "yutailang0119"
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private static var name: String = "yutailang0119"
+
+          #if DEBUG
+          static
+          var ___name: String {
+            get {
+              name
+            }
+            set {
+              name = newValue
+            }
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
 }
