da16e31700c911606b245922011b2fe8892436ea
yutailang0119 <yutailang0119@gmail.com>
2024-11-12 23:51:07 +0900

    Fix PropertyExpansionTests to add testStored

--- Tests/BypassAccessingTests/Expansions/PropertyExpansionTests.swift
+++ Tests/BypassAccessingTests/Expansions/PropertyExpansionTests.swift
@@ -2,4 +2,57 @@ import SwiftSyntaxMacroExpansion
 import SwiftSyntaxMacrosTestSupport
 import XCTest
 
-final class PropertyExpansionTests: XCTestCase {}
+final class PropertyExpansionTests: XCTestCase {
+  func testStored() throws {
+    #if canImport(BypassAccessingMacros)
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private let name: String = "yutailang0119"
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private let name: String = "yutailang0119"
+
+          #if DEBUG
+          var ___name: String {
+            name
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+
+    assertMacroExpansion(
+      """
+      struct User {
+        @BypassAccess
+        private var name: String = "yutailang0119"
+      }
+      """,
+      expandedSource: """
+        struct User {
+          private var name: String = "yutailang0119"
+
+          #if DEBUG
+          var ___name: String {
+            get {
+              name
+            }
+            set {
+              name = newValue
+            }
+          }
+          #endif
+        }
+        """,
+      macros: testMacros
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+}
