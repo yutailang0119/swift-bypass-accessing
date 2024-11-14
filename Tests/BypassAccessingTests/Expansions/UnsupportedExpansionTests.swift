96fe389c11fce8b7ee4b974517c0aa55e4cd0da8
yutailang0119 <yutailang0119@gmail.com>
2024-11-15 00:16:12 +0900

    Add UnsupportedExpansionTests

---
+++ Tests/BypassAccessingTests/Expansions/UnsupportedExpansionTests.swift
@@ -0,0 +1,36 @@
+import SwiftSyntaxMacroExpansion
+import SwiftSyntaxMacrosTestSupport
+import XCTest
+
+final class UnsupportedExpansionTests: XCTestCase {
+  func testUnsupportedDeclaration() throws {
+    #if canImport(BypassAccessingMacros)
+    assertMacroExpansion(
+      """
+      @BypassAccess
+      struct User {}
+      """,
+      expandedSource: """
+        struct User {}
+        """,
+      diagnostics: [
+        DiagnosticSpec(
+          message: "'@BypassAccess' cannot be applied to this declaration",
+          line: 1,
+          column: 1,
+          severity: .error
+        ),
+        DiagnosticSpec(
+          message: "'@BypassAccess' cannot be applied to this declaration",
+          line: 1,
+          column: 1,
+          severity: .error
+        ),
+      ],
+      macros: testMacros
+    )
+    #else
+    throw XCTSkip("macros are only supported when running tests for the host platform")
+    #endif
+  }
+}
